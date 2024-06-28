af44235a46e19c1e9a76662173e480e007b633a7
sulume <sulume@gmail.com>
2024-06-28 12:50:04 +0000

    encode

--- src/bin/tr.rs
+++ src/bin/tr.rs
@@ -1,14 +1,34 @@
+use clap::Parser;
 use std::io::Read;
 
-fn tr(c: char) -> char {
+fn decode(c: char) -> char {
     // TODO: make it a constnat
     let chars: Vec<_> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n".chars().collect();
     return chars[c as usize - 33];
 }
 
+fn encode(c: char) -> char {
+    // TODO: make it a constant
+    let chars: Vec<_> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n".chars().collect();
+    let index = chars.iter().position(|&x| x == c).unwrap();
+    return (index + 33) as u8 as char;
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    encode: bool,
+}
+
 fn main() {
+    let args = Args::parse();
     let mut s = String::new();
     std::io::stdin().read_to_string(&mut s).unwrap();
-    let s = s.chars().map(tr).collect::<String>();
+    let s = if args.encode {
+        s.chars().map(encode).collect::<String>()
+    } else {
+        s.chars().map(decode).collect::<String>()
+    };
     println!("{}", s);
 }
