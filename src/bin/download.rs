9a18e2a19fb9822dd7e84fc236f3f1e51c01eb41
Takuya Akiba <t.akiba.65536@gmail.com>
2024-06-29 00:10:15 +0900

    auto download + echoeval

--- src/bin/download.rs
+++ src/bin/download.rs
@@ -1,6 +1,8 @@
 use icfpc2024::encryption::get_bearer;
 use reqwest::blocking::Client;
 use std::io::Read;
+use std::thread;
+use std::time::Duration;
 
 fn decode(c: char) -> char {
     // TODO: make it a constnat
@@ -13,6 +15,37 @@ fn encode(c: char) -> char {
     let chars: Vec<_> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n".chars().collect();
     let index = chars.iter().position(|&x| x == c).unwrap();
     return (index + 33) as u8 as char;
+}
+
+fn echoeval(input: &str) -> anyhow::Result<String> {
+    eprintln!("--------------------------------------------------------------------------------");
+    eprintln!("Raw request:\n{}\n", &input);
+
+    let url = "https://boundvariable.space/communicate";
+    let client = Client::new();
+
+    let res = client
+        .post(url)
+        .body(r"B. S%#(/} ".to_string() + input)
+        .header(
+            "Authorization",
+            "Bearer 1b2a9024-2287-4eac-a58f-66a33726e529",
+        )
+        .send()?;
+
+    let body = res.text()?;
+    eprintln!("--------------------------------------------------------------------------------");
+    eprintln!("Raw response:\n{}\n", body);
+
+    let decoded_text = body.chars().skip(1).map(decode).collect::<String>();
+    eprintln!("--------------------------------------------------------------------------------");
+    eprintln!("Decoded response:\n{}\n", decoded_text);
+
+    let suffix = "\nYou scored some points for using the echo service!\n";
+    assert!(decoded_text.ends_with(suffix));
+    let decoded_text = decoded_text[..decoded_text.len() - suffix.len()].to_owned();
+
+    Ok(decoded_text)
 }
 
 fn request(input: &str) -> anyhow::Result<String> {
@@ -41,33 +74,25 @@ fn request(input: &str) -> anyhow::Result<String> {
     eprintln!("--------------------------------------------------------------------------------");
     eprintln!("Raw response:\n{}\n", body);
 
-    let decoded_text = body.chars().skip(1).map(decode).collect::<String>();
-    eprintln!("--------------------------------------------------------------------------------");
-    eprintln!("Decoded response:\n{}\n", decoded_text);
-
-    Ok(decoded_text)
+    if body.starts_with("B") {
+        thread::sleep(Duration::from_secs(3));
+        echoeval(&body)
+    } else {
+        let decoded_text = body.chars().skip(1).map(decode).collect::<String>();
+        eprintln!(
+            "--------------------------------------------------------------------------------"
+        );
+        eprintln!("Decoded response:\n{}\n", decoded_text);
+        Ok(decoded_text)
+    }
 }
 
 fn main() -> anyhow::Result<()> {
-    /*
-    let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input)?;
-    if input.ends_with('\n') {
-        input.pop();
-    }
-    // request(&input)?;
-    */
-
-    for i in 11..21 {
+    for i in 21..22 {
         let input = format!("get lambdaman{}", i);
         let output = request(&input)?;
-        std::fs::write(
-            format!(
-                "/Users/takiba/Dropbox/ICFPC2024/lambdaman/lambdaman{}.txt",
-                i
-            ),
-            output,
-        )?;
+        std::fs::write(format!("./input/lambdaman/lambdaman{}.txt", i), output)?;
+        thread::sleep(Duration::from_secs(3));
     }
 
     Ok(())
