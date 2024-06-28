91d912c0355f11695d8796747a7aff611721aa2c
imos <173752+imos@users.noreply.github.com>
2024-06-29 00:05:08 +0900

    .

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,6 @@ pub mod www;
 #[cfg(feature = "mysql")]
 pub mod sql;
 
-pub mod encryption;
-
 pub trait SetMinMax {
     fn setmin(&mut self, v: Self) -> bool;
     fn setmax(&mut self, v: Self) -> bool;
