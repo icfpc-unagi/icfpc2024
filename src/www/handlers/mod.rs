a0889277b12774e7ec13739637d4c25973a0d5e6
sulume <sulume@gmail.com>
2024-06-28 15:08:39 +0000

    dockerignore

--- src/www/handlers/mod.rs
+++ src/www/handlers/mod.rs
@@ -15,7 +15,7 @@ use actix_web::{web, HttpResponse, Responder};
 pub async fn index() -> impl Responder {
     HttpResponse::Ok()
         .content_type("text/html")
-        .body(www::handlers::template::render("Hello, world!"))
+        .body(www::handlers::template::render("Hello, world!<br><a href='/comm'>communicate</a>"))
 }
 use serde::Deserialize;
 
@@ -31,7 +31,10 @@ pub async fn comm(query: web::Query<CommQuery>) -> impl Responder {
     let client = Client::new();
     let res = client
         .post("https://boundvariable.space/communicate")
-        .header("Authorization", encryption::get_bearer())
+        .header(
+            "Authorization",
+            "Bearer 1b2a9024-2287-4eac-a58f-66a33726e529",
+        )
         .body(if query.raw {
             query.q.to_owned()
         } else {
@@ -46,15 +49,17 @@ pub async fn comm(query: web::Query<CommQuery>) -> impl Responder {
         .body(www::handlers::template::render(&format!(
             r#"
             <form>
-                <textarea name="q" placeholder="message" autofocus required cols="160"></textarea>
+                <textarea name="q" placeholder="message" autofocus required cols="160">{}</textarea>
                 <div>
-                    <input type="checkbox" name="raw" id="raw"><label for="raw">raw</label>
+                    <input type="checkbox" name="raw" id="raw"{}><label for="raw">raw</label>
                     <button type="submit">Send</button>
                 </div>
             </form>
             <textarea placeholder="response" readonly cols="160" rows="10">{}</textarea>
             <textarea placeholder="response" readonly cols="160" rows="50">{}</textarea>
             "#,
+            query.q,
+            if query.raw { " checked" } else { "" },
             body,
             decode(&body),
         )))
