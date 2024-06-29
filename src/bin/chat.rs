fab4aa25ef008c03201f5a1a7b7b3d4a339b3a35
Takuya Akiba <t.akiba.65536@gmail.com>
2024-06-29 10:16:59 +0900

    chat decode

--- src/bin/chat.rs
+++ src/bin/chat.rs
@@ -1,6 +1,70 @@
 use icfpc2024::*;
 use std::io::Read;
 
+use std::thread;
+use std::time::Duration;
+
+use icfpc2024::communicate;
+
+fn decode(c: char) -> char {
+    // TODO: make it a constnat
+    let chars: Vec<_> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n".chars().collect();
+    return chars[c as usize - 33];
+}
+
+fn encode(c: char) -> char {
+    // TODO: make it a constant
+    let chars: Vec<_> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n".chars().collect();
+    let index = chars.iter().position(|&x| x == c).unwrap();
+    return (index + 33) as u8 as char;
+}
+
+fn echoeval(input: &str) -> anyhow::Result<String> {
+    eprintln!("--------------------------------------------------------------------------------");
+    eprintln!("Raw request:\n{}\n", &input);
+
+    let body = communicate(r"B. S%#(/} ".to_string() + input)?;
+
+    eprintln!("--------------------------------------------------------------------------------");
+    eprintln!("Raw response:\n{}\n", body);
+
+    let decoded_text = body.chars().skip(1).map(decode).collect::<String>();
+    eprintln!("--------------------------------------------------------------------------------");
+    eprintln!("Decoded response:\n{}\n", decoded_text);
+
+    let suffix = "\nYou scored some points for using the echo service!\n";
+    assert!(decoded_text.ends_with(suffix));
+    let decoded_text = decoded_text[..decoded_text.len() - suffix.len()].to_owned();
+
+    Ok(decoded_text)
+}
+
+fn request(input: &str) -> anyhow::Result<String> {
+    eprintln!("--------------------------------------------------------------------------------");
+    eprintln!("Raw request:\n{}\n", &input);
+
+    let text = "S".to_owned() + &input.chars().map(encode).collect::<String>();
+
+    eprintln!("--------------------------------------------------------------------------------");
+    eprintln!("Encoded request:\n{}\n", &text);
+
+    let body = communicate(text.to_string())?;
+    eprintln!("--------------------------------------------------------------------------------");
+    eprintln!("Raw response:\n{}\n", body);
+
+    if body.starts_with("B") {
+        thread::sleep(Duration::from_secs(3));
+        echoeval(&body)
+    } else {
+        let decoded_text = body.chars().skip(1).map(decode).collect::<String>();
+        eprintln!(
+            "--------------------------------------------------------------------------------"
+        );
+        eprintln!("Decoded response:\n{}\n", decoded_text);
+        Ok(decoded_text)
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input)?;
@@ -8,21 +72,7 @@ fn main() -> anyhow::Result<()> {
         input.pop();
     }
 
-    println!("--------------------------------------------------------------------------------");
-    println!("Raw request:\n{}\n", &input);
-
-    let text = "S".to_owned() + &encode_str(&input);
-
-    println!("--------------------------------------------------------------------------------");
-    println!("Encoded request:\n{}\n", &text);
-
-    let body = communicate(text.to_string())?;
-    println!("--------------------------------------------------------------------------------");
-    println!("Raw response:\n{}\n", body);
-
-    let decoded = decode(&body);
-    println!("--------------------------------------------------------------------------------");
-    println!("Decoded response:\n{}\n", decoded);
+    request(&input)?;
 
     Ok(())
 }
