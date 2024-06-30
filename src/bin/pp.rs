b1b2e2da40cb1af3045b67678b8b61d0576ece07
sulume <sulume@gmail.com>
2024-06-30 05:00:24 +0000

    print

--- src/bin/pp.rs
+++ src/bin/pp.rs
@@ -5,7 +5,8 @@ use icfpc2024::pp::preprocess;
 fn main() -> anyhow::Result<()> {
     let mut buf = String::new();
     std::io::stdin().read_to_string(&mut buf)?;
-    preprocess(&buf)?;
+    let s = preprocess(&buf)?;
+    println!("{}", s);
 
     Ok(())
 }
