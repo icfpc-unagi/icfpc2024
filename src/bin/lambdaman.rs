673ef69d3b0c0d1359fe271b40db3ceaad89c4dd
imos <173752+imos@users.noreply.github.com>
2024-06-30 00:26:20 +0000

    .

--- src/bin/lambdaman.rs
+++ src/bin/lambdaman.rs
@@ -1,19 +1,21 @@
 use clap::Parser;
 use icfpc2024::*;
+use std::io::prelude::*;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
     problem_id: i64,
-
-    #[arg(short, long)]
-    moves: String,
 }
 
 fn main() {
     let args = Args::parse();
-    let board = lambdaman::simulate_with_problem_id(args.problem_id, args.moves.as_str());
+    let mut moves = String::new();
+    for line in std::io::stdin().lock().lines() {
+        moves += &line.unwrap();
+    }
+    let board = lambdaman::simulate_with_problem_id(args.problem_id, moves.as_str());
     for row in board {
         println!("{}", row.iter().collect::<String>());
     }
