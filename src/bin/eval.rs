df9f0bc14923a5538bb0f2513dadda7f12f820dd
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2024-07-01 15:59:33 +0900

    feat: enrich eval/lambdaman (#27)

--- src/bin/eval.rs
+++ src/bin/eval.rs
@@ -2,35 +2,8 @@ use icfpc2024::*;
 use std::io::prelude::*;
 
 fn main() {
-    eprintln!("Enter a program to evaluate (end with ';'):");
-    let mut program = String::new();
-    for line in std::io::stdin().lock().lines() {
-        let line = line.unwrap();
-        let line = line.trim();
-        program += line;
-        program += "\n";
-        if program.trim().ends_with(';') {
-            let term = program.trim().trim_end_matches(';').to_owned();
-            program.clear();
-            match eval::transpile(&(term)) {
-                Ok(transpiled) => {
-                    eprintln!("Transpiled ({}): {}", transpiled.len(), transpiled);
-                }
-                Err(err) => {
-                    eprintln!("Error: {}", err);
-                    let result = eval::prettify(&term);
-                    eprintln!("Prettified:\n{}", result.0.trim());
-                    continue;
-                }
-            }
-            match eval::eval(&term) {
-                Ok(result) => {
-                    println!("{}", result);
-                }
-                Err(err) => {
-                    eprintln!("Error: {}", err);
-                }
-            }
-        }
+    loop {
+        let (_, value) = eval::input_eval();
+        println!("{}", value);
     }
 }
